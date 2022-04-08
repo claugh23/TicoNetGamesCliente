a97107d950b96a990050491899921730d2b03d64
cesartoshiro <48112790+cesartoshiro@users.noreply.github.com>
2022-04-07 19:56:55 -0600

    DashBoard Principal

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tico-net-games-client'),
+      dir: require('path').join(__dirname, './coverage/games'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponetsconfigModule } from './components/componetsconfig.module';
-import { NavigationComponent } from './Navigation/navigation/navigation.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ComponetsconfigModule
+    BrowserModule
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
