a30ecae07ff3838a874f9dab3725f70aff9d4c4f
Gabriel0697 <70977014+Gabriel0697@users.noreply.github.com>
2022-05-03 14:36:35 -0600

    Modificaciones al modal de compra de juego. Falta logica en el component

--- src/app/components/Games/games-page/games-page.component.ts
+++ src/app/components/Games/games-page/games-page.component.ts
@@ -14,8 +14,8 @@ import { Router } from '@angular/router';
 export class GamesPageComponent implements OnInit {
   SelectedGame: GamesModel[] = [];
   GameId: any = 0;
-  FormAddGame: FormGroup;
-  FormEditGame: FormGroup;
+  // FormAddGame: FormGroup;
+  FormBuyGame: FormGroup;
 
   StateUserForm: boolean = false;
   StateGamesForm: boolean = true;
@@ -28,18 +28,18 @@ export class GamesPageComponent implements OnInit {
     private FormEditBuilder: FormBuilder,
     private Ticonetserver: GamesServicesService) {
 
-    this.FormAddGame = this.FormGameBuilder.group({
+    // this.FormAddGame = this.FormGameBuilder.group({
+    //
+    //   form_gameName: new FormControl(),
+    //   form_gameDescription: new FormControl(),
+    //   form_gameCategory: new FormControl(),
+    //   form_gameSize: new FormControl(),
+    //   form_gamePrice: new FormControl(),
+    //   form_gameImageURL: new FormControl()
+    //
+    // });
 
-      form_gameName: new FormControl(),
-      form_gameDescription: new FormControl(),
-      form_gameCategory: new FormControl(),
-      form_gameSize: new FormControl(),
-      form_gamePrice: new FormControl(),
-      form_gameImageURL: new FormControl()
-
-    });
-
-    this.FormEditGame = this.FormEditBuilder.group({
+    this.FormBuyGame = this.FormEditBuilder.group({
       formEdit_name: new FormControl(),
       formEdit_description: new FormControl(),
       formEdit_category: new FormControl(),
@@ -54,24 +54,24 @@ export class GamesPageComponent implements OnInit {
     this.SelectedGame.pop();
     this.SelectedGame.push(game);
 
-    this.FormEditGame.patchValue({formEdit_name: this.SelectedGame[0].gameName})
-    this.FormEditGame.patchValue({formEdit_description: this.SelectedGame[0].gameDescription})
-    this.FormEditGame.patchValue({formEdit_category: this.SelectedGame[0].gameCategory})
-    this.FormEditGame.patchValue({formEdit_price: this.SelectedGame[0].gamePrice})
-    this.FormEditGame.patchValue({formEdit_size: this.SelectedGame[0].gameSize})
+    this.FormBuyGame.patchValue({formEdit_name: this.SelectedGame[0].gameName})
+    this.FormBuyGame.patchValue({formEdit_description: this.SelectedGame[0].gameDescription})
+    this.FormBuyGame.patchValue({formEdit_category: this.SelectedGame[0].gameCategory})
+    this.FormBuyGame.patchValue({formEdit_price: this.SelectedGame[0].gamePrice})
+    this.FormBuyGame.patchValue({formEdit_size: this.SelectedGame[0].gameSize})
 
   }
 
-  UpdateGames(updates: GamesModel) {
+  BuyGame(updates: GamesModel) {
 
     const finalUpdate: GamesModel = {
       _id: this.SelectedGame[0]._id,
-      gameName: this.FormEditGame.get('formEdit_name')?.value,
-      gameDescription: this.FormEditGame.get('formEdit_description')?.value,
-      gameCategory: this.FormEditGame.get('formEdit_category')?.value,
+      gameName: this.FormBuyGame.get('formEdit_name')?.value,
+      gameDescription: this.FormBuyGame.get('formEdit_description')?.value,
+      gameCategory: this.FormBuyGame.get('formEdit_category')?.value,
       gameImage: this.SelectedGame[0].gameImage,
-      gamePrice: this.FormEditGame.get('formEdit_price')?.value,
-      gameSize: this.FormEditGame.get('formEdit_size')?.value
+      gamePrice: this.FormBuyGame.get('formEdit_price')?.value,
+      gameSize: this.FormBuyGame.get('formEdit_size')?.value
     }
 
     this.Ticonetserver.PutGames(finalUpdate).subscribe((result: any) => {
@@ -83,49 +83,49 @@ export class GamesPageComponent implements OnInit {
     })
   }
 
-  AddGame() {
+  // AddGame() {
+  //
+  //   const game: GamesModel = {
+  //     _id: Math.random() * 10000000,
+  //     gameName: this.FormAddGame.get('form_gameName')?.value,
+  //     gameDescription: this.FormAddGame.get('form_gameDescription')?.value,
+  //     gameCategory: this.FormAddGame.get('form_gameCategory')?.value,
+  //     gameSize: this.FormAddGame.get('form_gameSize')?.value,
+  //     gamePrice: Number.parseInt(this.FormAddGame.get('form_gamePrice')?.value),
+  //     gameImage: this.FormAddGame.get('form_gameImageURL')?.value
+  //   }
+  //
+  //   this.Ticonetserver.PostGame(game).subscribe((result): any => {
+  //
+  //     alert(JSON.stringify(result));
+  //     this.LoadGameList();
+  //
+  //   }, (error: HttpErrorResponse) => {
+  //
+  //     alert("Ocurrio un error al generar el juego" + '\n' + JSON.stringify(error))
+  //   })
+  // }
 
-    const game: GamesModel = {
-      _id: Math.random() * 10000000,
-      gameName: this.FormAddGame.get('form_gameName')?.value,
-      gameDescription: this.FormAddGame.get('form_gameDescription')?.value,
-      gameCategory: this.FormAddGame.get('form_gameCategory')?.value,
-      gameSize: this.FormAddGame.get('form_gameSize')?.value,
-      gamePrice: Number.parseInt(this.FormAddGame.get('form_gamePrice')?.value),
-      gameImage: this.FormAddGame.get('form_gameImageURL')?.value
-    }
-
-    this.Ticonetserver.PostGame(game).subscribe((result): any => {
-
-      alert(JSON.stringify(result));
-      this.LoadGameList();
-
-    }, (error: HttpErrorResponse) => {
-
-      alert("Ocurrio un error al generar el juego" + '\n' + JSON.stringify(error))
-    })
-  }
-
-  DeleteGameNow() {
-
-    //this.GameId = this.SelectedGame.find()
-
-    //console.log(idNUmber);
-    this.GameId = this.SelectedGame[0]._id;
-
-    console.log(this.GameId);
-
-
-    this.Ticonetserver.DeleteGame(this.GameId).subscribe((result): any => {
-
-      alert(JSON.stringify(result))
-
-
-    }, (errorDelete: HttpErrorResponse) => {
-
-      alert(JSON.stringify(errorDelete))
-    })
-  }
+  // DeleteGameNow() {
+  //
+  //   //this.GameId = this.SelectedGame.find()
+  //
+  //   //console.log(idNUmber);
+  //   this.GameId = this.SelectedGame[0]._id;
+  //
+  //   console.log(this.GameId);
+  //
+  //
+  //   this.Ticonetserver.DeleteGame(this.GameId).subscribe((result): any => {
+  //
+  //     alert(JSON.stringify(result))
+  //
+  //
+  //   }, (errorDelete: HttpErrorResponse) => {
+  //
+  //     alert(JSON.stringify(errorDelete))
+  //   })
+  // }
 
   LoadGameList() {
 
