ceddfec196da7f1e24fdb9d2ad49d9bb0882f0b8
Claudio Gonzalez Hernandez <40004826+claugh23@users.noreply.github.com>
2023-12-26 19:58:49 -0600

    Update shopping-cart.component.ts

--- src/app/components/Cart/shopping-cart.component.ts
+++ src/app/components/Cart/shopping-cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingModel } from "../../Interfaces/ShoppingDTO";
 import { GamesModel } from "../../Interfaces/GamesDTO";
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { GamesServicesService } from "../../Services/Games/games-services.service";
 import { HttpErrorResponse } from "@angular/common/http";
 
@@ -14,8 +14,8 @@ export class ShoppingCartComponent implements OnInit {
 
   SelectedGame: GamesModel[] = [];
   GameId: any = 0;
-  FormAddGame: UntypedFormGroup;
-  FormEditGame: UntypedFormGroup;
+  FormAddGame: FormGroup;
+  FormEditGame: FormGroup;
 
   StateUserForm: boolean = false;
   StateGamesForm: boolean = true;
@@ -23,27 +23,27 @@ export class ShoppingCartComponent implements OnInit {
 
 
   constructor(
-    private FormGameBuilder: UntypedFormBuilder,
-    private FormEditBuilder: UntypedFormBuilder,
+    private FormGameBuilder: FormBuilder,
+    private FormEditBuilder: FormBuilder,
     private Ticonetserver: GamesServicesService) {
 
     this.FormAddGame = this.FormGameBuilder.group({
 
-      form_gameName: new UntypedFormControl(),
-      form_gameDescription: new UntypedFormControl(),
-      form_gameCategory: new UntypedFormControl(),
-      form_gameSize: new UntypedFormControl(),
-      form_gamePrice: new UntypedFormControl(),
-      form_gameImageURL: new UntypedFormControl()
+      form_gameName: new FormControl(),
+      form_gameDescription: new FormControl(),
+      form_gameCategory: new FormControl(),
+      form_gameSize: new FormControl(),
+      form_gamePrice: new FormControl(),
+      form_gameImageURL: new FormControl()
 
     });
 
     this.FormEditGame = this.FormEditBuilder.group({
-      formEdit_name: new UntypedFormControl(),
-      formEdit_description: new UntypedFormControl(),
-      formEdit_category: new UntypedFormControl(),
-      formEdit_size: new UntypedFormControl(),
-      formEdit_price: new UntypedFormControl()
+      formEdit_name: new FormControl(),
+      formEdit_description: new FormControl(),
+      formEdit_category: new FormControl(),
+      formEdit_size: new FormControl(),
+      formEdit_price: new FormControl()
     })
 
   }
